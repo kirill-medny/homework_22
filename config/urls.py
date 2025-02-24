9b4e1ff9869cb5169b88022cfc4648d682ac1fcf
Kirill_Medny <medny.kirill@yandex.ru>
2025-02-24 13:19:02 +0300

    add readme

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-from django.utils.translation.trans_real import catalog
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",include("catalog.urls", namespace="catalog" ))
+    path("", include("catalog.urls", namespace="catalog")),
 ]
